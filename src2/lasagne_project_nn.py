8ee7f4491efd03155353337f2f91d520e142568c
Elias Hussen <elsnyc@gmail.com>
2016-05-04 19:21:44 -0400

    errors function binary

--- src2/lasagne_project_nn.py
+++ src2/lasagne_project_nn.py
@@ -765,4 +765,4 @@ def errors(pred, y, threshold=0.5):
     """Computes the binary accuracy between predictions and targets.
     """
     predictions = theano.tensor.ge(pred, threshold)
-    return theano.tensor.eq(predictions, targets)
+    return T.mean(T.neq(predictions, targets))
