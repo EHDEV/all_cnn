d22d75bdadc7ebe0632f15ecbe3e5864184c1fa5
Ubuntu <ubuntu@ip-172-31-52-64.ec2.internal>
2016-05-06 00:51:02 +0000

    Check dis out

--- src2/lasagne_experiments.py
+++ src2/lasagne_experiments.py
@@ -13,13 +13,13 @@ import timeit
 import inspect
 from lasagne_project_nn import categorical_accuracy, all_CNN_C, train_nn
 
-def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
+def run_experiment(lr=0.1, n_epochs=20, nkerns=[96, 192, 10],
              batch_size=200, verbose=False, kernel_shape=(3, 3)):
     """
     Wrapper function for testing Multi-Stage ConvNet on SVHN dataset
 
-    :type learning_rate: float
-    :param learning_rate: learning rate used (factor for the stochastic
+    :type lr: float
+    :param lr: learning rate used (factor for the stochastic
     gradient)
 
     :type n_epochs: int
@@ -61,12 +61,16 @@ def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
     channel = 3
     imsize = 32
 
-    num_epochs = 12
+    data_size = X_train.eval().shape[0]
+    tdata_size = X_test.eval().shape[0]
+    vdata_size = X_val.eval().shape[0]
+    
+    num_epochs = 50
     lambda_decay = 1e-3
 
-    X_train = X_train.reshape((9000, 3, imsize, imsize))
-    X_test = X_test.reshape((10000, 3, imsize, imsize))
-    X_val = X_val.reshape((1000, 3, imsize, imsize))
+    X_train = X_train.reshape((data_size, 3, imsize, imsize))
+    X_test = X_test.reshape((tdata_size, 3, imsize, imsize))
+    X_val = X_val.reshape((vdata_size, 3, imsize, imsize))
 
     network = all_CNN_C(x)
     
@@ -80,7 +84,7 @@ def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
 
     params = lasagne.layers.get_all_params(network, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
-        train_loss, params, learning_rate=0.01, momentum=0.9)
+        train_loss, params, learning_rate=lr, momentum=0.9)
 
     val_prediction = lasagne.layers.get_output(network)
     val_loss = categorical_accuracy(val_prediction, y)
@@ -93,8 +97,8 @@ def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
 
     test_loss = test_loss.mean()
     # As a bonus, also create an expression for the classification accuracy:
-    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), y),
-                      dtype=theano.config.floatX)
+    #test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), y),
+    #                  dtype=theano.config.floatX)
 
     # Compile a function performing a training step on a mini-batch (by giving
     # the updates dictionary) and returning the corresponding training loss:
@@ -118,7 +122,7 @@ def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
 
     test_fn = theano.function(
         [index],
-        [test_loss, test_acc],
+        test_loss,
         givens={
             x: X_test[index * batch_size: (index + 1) * batch_size],
             y: y_test[index * batch_size: (index + 1) * batch_size]
@@ -131,4 +135,7 @@ def run_experiment(learning_rate=0.1, n_epochs=20, nkerns=[96, 192, 10],
 
 
 if __name__ == "__main__":
-    run_experiment(verbose=True)
+    
+    #if len(sys.argv) > 1:
+    run_experiment(lr = 0.1, batch_size=200, verbose=True)
+    
